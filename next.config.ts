c46fd4d8add45f5221fb2da284ca26db5b0fda9a
Coiness <1512133221@qq.com>
2025-12-12 20:57:06 +0800

    fix:pwa绝对是最后一次。。。

--- next.config.ts
+++ next.config.ts
@@ -3,25 +3,7 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
   disable: process.env.NODE_ENV === "development",
-  workboxOptions: {
-    disableDevLogs: true,
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'offlineCache',
-          expiration: {
-            maxEntries: 200,
-          },
-        },
-      },
-    ],
-  },
 });
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
