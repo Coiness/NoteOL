e71a325e7c5b359dcdcc94e0295c438061c82160
Coiness <1512133221@qq.com>
2025-12-12 21:25:11 +0800

    重新配置了next.config

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,47 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 const nextConfig: NextConfig = {
-  /* config options here */
   turbopack: {},
+  
+  async headers() {
+    return [
+      {
+        source: '/sw.js',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/javascript; charset=utf-8',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate',
+          },
+          {
+            key: 'Service-Worker-Allowed',
+            value: '/', 
+          }
+        ],
+      },
+      {
+        source: '/manifest.json',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json; charset=utf-8',
+          }
+        ]
+      }
+    ];
+  },
+  
+  async rewrites() {
+    return [
+      {
+        source: '/sw.js',
+        destination: '/sw.js',
+      },
+    ];
+  },
 };
 
 export default withBundleAnalyzer(nextConfig);
